3af13ee1b4f46d482c91038aba0113d56fefce60
Sean <99051838+FaraamK@users.noreply.github.com>
2022-03-30 17:43:23 -0400

    Update main.sql

--- backend/main.sql
+++ backend/main.sql
@@ -1,6 +1,6 @@
 CREATE TABLE PURCHASE(
 ORDERID VARCHAR(25) PRIMARY KEY
-);
+); -- Table to track Order IDs for USERS table
 
 -- VARCHAR2 is not a valid data type in MySQL
 
@@ -11,31 +11,31 @@ FNAME VARCHAR (25) NOT NULL,
 LNAME VARCHAR (25) NOT NULL, 
 SIGNUP_DATE DATE,
 USER_KEY VARCHAR (25) NOT NULL
-); 
+); -- Table to hold user data such as login info, signup date, and userkey. 
 
---DATE DEFAULT SYSDATE is not acceptable format for MySQL, just use DATE
+-- DATE DEFAULT SYSDATE is not acceptable format for MySQL, just use DATE
 
 CREATE TABLE MOVIES (
 TITLE VARCHAR2 (25) PRIMARY KEY,
 MOVIE_KEY VARCHAR2(25) NOT NULL,
 GENRE VARCHAR2 (20) NOT NULL, 
 LENGHT VARCHAR2 (300) NOT NULL
-);
+); -- Holds data about the movie, from lenght, to genre and title. 
 
 ALTER TABLE USERS ADD ORDERID VARCHAR (25) NOT NULL; 
 
 ALTER TABLE USERS ADD CONSTRAINT FOREIGN KEY (ORDERID) 
 REFERENCES PURCHASE (ORDERID);
 
---Updated foreign key to USERS table
+-- Updated foreign key to USERS table
 
-ALTER TABLE MOVIES ADD PRICE VARCHAR (6) NOT NULL;
+ALTER TABLE MOVIES ADD PRICE VARCHAR (6) NOT NULL; -- adds movie price row to show price of the film
 
-ALTER TABLE PURCHASE ADD USERNAME VARCHAR(10) NOT NULL;
-ALTER TABLE PURCHASE ADD PRICE VARCHAR(6) NOT NULL;
+ALTER TABLE PURCHASE ADD USERNAME VARCHAR(10) NOT NULL; -- adds username to table so username can be tracked to orderid
+ALTER TABLE PURCHASE ADD PRICE VARCHAR(6) NOT NULL; -- adds price to PURCHASE table to show price on table
 
---Used phpMyAdmin to input values into table to test php code 
+-- Used phpMyAdmin to input values into table to test php code, held in default_values file
 
 ALTER TABLE PURCHASE DROP FOREIGN KEY USERNAME;
 
--- LAST EDITED BY SEAN SHEA, WED MARCH 30TH, 5:07 PM EST
+-- LAST EDITED BY SEAN SHEA, WED MARCH 30TH, 5:43 PM EST 
