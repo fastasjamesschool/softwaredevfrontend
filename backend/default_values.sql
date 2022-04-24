7594496c2124e0a113ee8bc9a858dce8bc31608d
Sean <99051838+FaraamK@users.noreply.github.com>
2022-04-24 16:36:51 -0400

    Update default_values.sql

--- backend/default_values.sql
+++ backend/default_values.sql
@@ -10,4 +10,10 @@ INSERT INTO `PURCHASE` (`ORDERID`, `USERNAME`, `PRICE`) VALUES ('00001', 'fastja
 INSERT INTO `USERS` (`USERNAME`, `PASSWD`, `EMAIL`, `FNAME`, `LNAME`, `SIGNUP_DATE`, `USER_KEY`, `ORDERID`) 
 VALUES ('fastjames', '123456', 'fastasjames@idk.com', 'James', 'Conlin', '2022-03-30', '0001', '00001');
 
+INSERT INTO `PURCHASE_INFO` (`CNAME`, `CNUM`, `CVV`, `EX_DATE`, `BADDR`, `USERNAME`) 
+VALUES ('Sean Shea', '9999999999999999', '000', '2026/4/24', '83 Windham St, Willimantic, CT 06226', 'fastjames')
+
 --above values are work in progress
+
+
+-- LAST EDITED BY SEAN SHEA APRIL 24th, 4:36 PM EST
