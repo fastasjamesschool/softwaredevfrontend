51023588f9d2a87be84e41a72d777ad5ed0a279e
fastasjames <garb76767@gmail.com>
2022-04-24 14:57:37 -0400

    Script is now good to be a default script for the database implementation

--- frontend/jamesdoingphpstuff/connectdatabase.php
+++ frontend/jamesdoingphpstuff/connectdatabase.php
@@ -4,12 +4,10 @@ $password = "";
 $database = "movie";
 $port = '8889';
 $charset = 'utf8mb4';
-$table = "USERS";
 
 try {
     $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
     echo "Connection works";
   } catch (PDOException $e) {
       print "Error!: " . $e->getMessage() . "<br/>";
-      die();
   }
