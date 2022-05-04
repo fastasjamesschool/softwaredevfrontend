784c3e0da0e6f0d3b20771168cde0a7f842696f0
PeterGion <Peter.j.Gionfriddo@gmail.com>
2022-05-04 00:38:27 -0400

    Minor sprucing up done to some of the pages

---
+++ frontend/account pages/Forgot Password/ForgotPassword.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    let button = document.getElementById("loginButton")
+    button.onclick = function(){
+        document.getElementById("text").innerHTML = "you will receive an email shortly";
+    }
+}
